fc87018c39d77ef16c403bd0b2710b1307a3abcd
Mert Adalı <mertadali605@gmail.com>
2023-12-10 20:14:15 +0300

    Layout design created

---
+++ app/src/main/java/com/mertadali/mapsactivity/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.mertadali.mapsactivity
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val menuInflater = menuInflater
+        menuInflater.inflate(R.menu.place_menu,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+           val intent = Intent(this,MapsActivity::class.java)
+            startActivity(intent)
+        return super.onOptionsItemSelected(item)
+    }
+}
--- app/src/main/java/com/mertadali/mapsactivity/MapsActivity.kt
+++ app/src/main/java/com/mertadali/mapsactivity/MapsActivity.kt
@@ -7,6 +7,7 @@ import android.location.LocationListener
 import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
@@ -155,6 +156,14 @@ private var trackBoolean : Boolean? = null
 
         selectedLatitude = p0.latitude
         selectedLongitude = p0.longitude
+
+    }
+
+    fun save(view : View){
+
+    }
+
+    fun delete(view: View){
 
     }
 
